017796913f1b7deec1d30181055ceea3987e222c
Omar G <dinodev.master@gmail.com>
2023-03-24 16:23:44 -0600

    back url update

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -10,7 +10,7 @@ export class UsersService {
 
   constructor(private readonly httpClient:HttpClient) { }
 
-  url: string = 'https://advantage-back-l2d8coybz-dinodevmaster.vercel.app'
+  url: string = 'https://advantage.onrender.com/'
 
   getUsers(): Observable<Users[]> {
     return this.httpClient.get<Users[]>(`${this.url}/users`)
